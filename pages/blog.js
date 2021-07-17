f4c70f0afb6ff8d63255bc1dc9b4fd5072661992
unknown <ahmad.rafi02@ui.ac.id>
2021-07-17 21:07:23 +0700

    page restructuring: blog and portfolio

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+
+
+function BlogPage() {
+  return (
+    <div>
+      <h1>The Blog Page</h1>
+    </div>
+  );
+}
+
+export default BlogPage;
---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+
+
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+}
+
+export default PortfolioPage;
