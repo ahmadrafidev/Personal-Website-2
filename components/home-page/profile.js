641254ddc759975db587279af8686918c709bd65
unknown <ahmad.rafi02@ui.ac.id>
2021-07-19 15:40:10 +0700

    body text

--- components/home-page/profile.js
+++ components/home-page/profile.js
@@ -1,6 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import styled from 'styled-components';
+
 
 import classes from './profile.module.css';
+
+const BlueLink = styled.a `
+  color: blue;
+`
 
 function Profile() {
   return(
@@ -13,10 +20,27 @@ function Profile() {
           height={300}
         />
       </div>
-      <h1>Hi, I am Rafi</h1>
+      <h1>Hi, I am Rafi!</h1>
+      <p>I am a sophomore computer science student at Universitas Indonesia</p>
       <p>
-        I am a sophomore computer science student at Universitas Indonesia
+        I am here to write story about my life in college
       </p>
+      <p>
+        <Link href="/blog" passHref>
+          <BlueLink href="/about">My Writings</BlueLink>
+        </Link>
+      </p>
+      <p>
+        <Link href="/portfolio" passHref>
+          <BlueLink href="/about">My Portfolios</BlueLink>
+        </Link>
+      </p>
+      <p>
+        <Link href="/about" passHref>
+          <BlueLink href="/about">More info about me...</BlueLink>
+        </Link>
+      </p>
+      
     </section>
   );
 }
