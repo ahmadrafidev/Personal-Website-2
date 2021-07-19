93ae97096a6761acbc21dbac1120a0ccb7347973
unknown <ahmad.rafi02@ui.ac.id>
2021-07-19 16:36:01 +0700

    adding icon in navigation

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -5,6 +5,7 @@
 import Logo from './logo';
 import classes from './main-navigation.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 function MainNavigation() {
@@ -18,21 +19,49 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
+            <Image 
+              src="/icons/home.png" 
+              width='25' 
+              height='25' 
+              alt='home picture'
+              quality='100'
+            />
             <Link href='/'>
               <a>Home</a>
             </Link>
           </li>
           <li>
+            <Image 
+              src="/icons/blog.png" 
+              width='25' 
+              height='25' 
+              alt='blog picture'
+              quality='100'
+            />
             <Link href='/blog'>
               <a>Blog</a>
             </Link>
           </li>
           <li>
+            <Image 
+              src="/icons/portfolio.png" 
+              width='25' 
+              height='25' 
+              alt='portfolio picture'
+              quality='100'
+            />
             <Link href='/portfolio'>
               <a>Portfolio</a>
             </Link>
           </li>
           <li>
+            <Image 
+              src="/icons/about.png" 
+              width='25' 
+              height='25' 
+              alt='about picture'
+              quality='100'
+            />
             <Link href='/about'>
               <a>About</a>
             </Link>
