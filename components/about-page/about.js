8164b5b33337ef6a5c10fe20c0eb16963a30003c
unknown <ahmad.rafi02@ui.ac.id>
2021-07-20 20:36:11 +0700

    making paragraph justify in about me

--- components/about-page/about.js
+++ components/about-page/about.js
@@ -14,28 +14,24 @@ function AboutMe() {
           width: "70%",
         }}
       />
-      <h1 className={classes.header}>
-        About Me
-      </h1>
-      <div>
-        <p>
-          My name is Ahmad Rafi Wirana. Currently, I am a sophomere
-          Computer Science student at University of Indonesia.
-        </p>
-        <p>
-          I am a software development, data science, ethical hacking, competitive programming enthusiast.
-        </p>
-        <p>
-          Other than college, I usually do some side projects
-          including website development and mobile development.
-        </p>
-        <p>
-          My projects cleanly arranged in <Link passHref href="/portfolio"><BlueLink href="/portfolio">portfolio page</BlueLink></Link>.
-        </p>
-        <p>
-          Reach me out on email: <BlueLink><a href="mailto:ahmadrafi9910@gmail.com">Send email</a></BlueLink>
-        </p>
-      </div>
+      <header className={classes.header}>
+        <h1>
+          About Me
+        </h1>
+      </header>
+      <body className={classes.body}>
+        <div>
+          <p>
+            My name is Ahmad Rafi Wirana. Currently, I am a sophomere
+            Computer Science student at University of Indonesia.  
+            I am a software development, data science, ethical hacking, and competitive programming enthusiast.
+            Other than college, I usually do some side projects
+            including website development and mobile development.
+            My projects cleanly arranged in <Link passHref href="/portfolio"><BlueLink href="/portfolio">portfolio page</BlueLink></Link>.
+            Reach me out on email: <BlueLink><a href="mailto:ahmadrafi9910@gmail.com">send email.</a></BlueLink>
+          </p>
+        </div>
+      </body>
       <hr 
         style={{
           width: "70%",
