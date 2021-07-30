497b69f1b9a92484717dcd67e8a0c98eeff66ab9
unknown <ahmad.rafi02@ui.ac.id>
2021-07-31 01:12:21 +0700

    Adding meta description

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
                 <meta charSet="utf-8" />
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="description" content="A stories from Ahmad Rafi that are packaged in the form of a website"/>
                 <html lang="en"/>
             </Head>
             <Component {...pageProps }/>
